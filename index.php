50b9823fe465ba6914b56a35947ded6e08ef3965
nurkhairiq23 <58208306+nurkhairiq23@users.noreply.github.com>
2020-09-22 00:30:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+</style>
+</head>
 <body>
 
-<h1>My First Heading</h1>
+<h1>Fasilitas UIN Syarif Hidayatullah</h1>
 
-<p>My first paragraph.</p>
+<p>bla bla</p>
 
 </body>
 </html>
