2a7d9a740600c23062472c2c6e2f67c0b2afcadc
nurkhairiq23 <58208306+nurkhairiq23@users.noreply.github.com>
2020-09-23 00:36:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,10 +18,22 @@ p {
 </style>
 </head>
 <body>
-
-<h1>Fasilitas UIN Syarif Hidayatullah</h1>
-
-<p>bla bla</p>
-
+<div class="wrap">
+    <div class="header">          
+            <h1>Cloud Computing</h1>
+    </div>
+ 
+<div class="content"><center>
+            <h4>PERPUSTAKAAN UTAMA UIN SYARIF HIDYATULLAH JAKARTA</h4>
+            <img src="perpustakaan.jpg" height= "250px" width= "500px">
+            <p style="text-align: center">Perpustakaan utama UIN Jakarta ini bisa dibilang gedung baru. Perpustakaan-nya 
+            sangat bersih dan juga sangat nyaman. Dan untuk isi buku Perpustakaan-nya juga lumayan lengkap. Sangat nyaman untuk mengerjakan tugas di sana.</p>
+            <h4>STUDY FROM HOME</h4>
+            <p style="text-align: center">Nama nya juga study from home, udah pasti ada positif dan negatifnya. positifnya ya kita bisa mengecilkan
+            resiko penyebaran virus ini, kita juga bisa melatih team work secara berjauhan (tugas kelompok). kalo negatifnya sih jadi kurang produktif,
+            jadi semakin susah juga buat memahami materi yang diberika dosen, jadi kurang konsentrasi juga untuk belajar karena suasana rumah, dan 
+            tentunya masih banyak lagi hal lainnya mengenai suka duka dari study from home ini.</p>
+        </center></div>
+</div>
 </body>
 </html>
