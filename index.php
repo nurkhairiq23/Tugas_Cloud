8ea96cb28116709345aef3b9cd7d6383bc6f06ea
nurkhairiq23 <58208306+nurkhairiq23@users.noreply.github.com>
2020-09-24 16:35:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -104,23 +104,23 @@
             <h2>FASILITAS UIN SYARIF HIDAYATULLAH</h2>
 		
             <h3 style="font-family: serif">1. Perpustakaan Utama UIN Jakarta</h3>
-	    <img src="perpustakaan.jpg" height= "250px" width= "500px">
+	    <img src="perpustakaan.jpg" height= "200px" width= "400px">
             <p style="text-align: center"> Perpustakaan utama UIN Jakarta ini atau dapat disebut juga Pusat Perpustakaan UIN Jakarta merupakan salah satu fasilitas yang ada di UIN jakarta. Perpustakaan ini terdiri dari tujuh lantai dengan dilengkapi buku-buku umum, buku-buku Islam, kumpulan skripsi, tesis, disertasi dan jurnal-jurnal ilmiah serta buku-buku online terlengkap. Pelayanannya yang baik dan canggih juga membuat mahasiswa nyaman untuk belajar di Perpustakaan Pusat. Selain itu di tiap fakultas juga dilengkapi  dengan Perpustakaan Fakultas dan gedung khusus Perpustakaan Pascasarjana.</p>
 
             <h3 style="font-family: serif">2. Pusat Laboratorium Terpadu UIN Jakarta</h3>		
-	    <img src="plt.jpg" height= "250px" width= "500px">
+	    <img src="plt.jpg" height= "200px" width= "400px">
             <p style="text-align: center"> Pusat Laboratorium Terpadu Universitas Islam Negeri Syarif Hidayatullah Jakarta. disiapkan untuk menyelenggarakan praktikum, sehingga selain dibekali pengetahuan teori di bangku kuliah, setiap mahasiswa juga dibekali keterampilan praktek, sehingga dapat dihasilkan sarjana yang mumpuni secara akademis dan mempunyai keterampilan yang kompetitif dan profesional.</p>
 
             <h3 style="font-family: serif">3. Student Center UIN Jakarta</h3>		
-	    <img src="sc.jpg" height= "250px" width= "500px">
+	    <img src="sc.jpg" height= "200px" width= "400px">
 	    <p style="text-align: center"> Student Center merupakan tempatnya para mahasiswa berkumpul. Gedung SC ini merupakan sebuah gedung yang luas, yang di dalamnya terdapat stadion mini beserta lapangannya, mesjid, aula, koperasi, hingga sekretariat UKM. Pilihan UKM yang ada di UIN Jakarta pun beragam, yang menyesuaikan dengan minat dan bakat Quipperian.</p>
 
             <h3 style="font-family: serif">4. Student Center UIN Jakarta</h3>		
-	    <img src="cangkir.jpg" height= "250px" width= "500px">
+	    <img src="cangkir.jpg" height= "200px" width= "400px">
 	    <p style="text-align: center"> Kafe yang berbentuk cangkir raksasa ini menyediakan menu-menu yang beraneka macam seperti aneka nasi, sajian mie yang beragam, ayam, ikan, lauk pauk, dan lain-lain. Selain makanan, Kafe Cangkir juga dilengkapi minimarket yang menjual kebutuhan mahasiswa yang lengkap, baik berupa alat tulis hingga keperluan rumah.</p>
 	
             <h3 style="font-family: serif">5. Auditorium Harun Nasution UIN Jakarta</h3>		
-	    <img src="harun.jpg" height= "250px" width= "500px">
+	    <img src="harun.jpg" height= "200px" width= "400px">
 	    <p style="text-align: center"> Auditorium Harun Nasution digunakan untuk tempat menyelenggarakan kegiatan-kegiatan besar seperti seminar, konser, hingga prosesi wisuda. </p>	    
 
             <h2>STUDY FROM HOME</h2>
