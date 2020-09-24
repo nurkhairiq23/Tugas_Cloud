e3f113dd56991d0ce81619f56164b06fab911551
nurkhairiq23 <58208306+nurkhairiq23@users.noreply.github.com>
2020-09-24 11:43:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 <html>
 <head>
   <title>Tugas Cloud Nurkhairi Amini</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
     body {
         background-color: pink;
